35d20b4fd3d18a6e39f2dd0eedc0451740286941
Chaitanya-vikas <chaitanyavikas52@gmail.com>
2026-02-24 01:13:43 +0530

    final project tested locally -- Ready to deploy

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8kj)b2n(kz40j_me)6(^*uy28xb&wm@g^n#ovenp=vy!j^u1_7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# 2. Allow any server (like Render) to host your app
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- pdf_chat/views.py
+++ pdf_chat/views.py
@@ -1,3 +1,31 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .utils import extract_text_from_pdf, get_ai_response
 from django.shortcuts import render
 
-# Create your views here.
+# Add this right above your chat_with_pdf API view
+def index(request):
+    return render(request, 'index.html')
+
+@api_view(['POST'])
+def chat_with_pdf(request):
+    pdf_file = request.FILES.get('document')
+    user_query = request.data.get('question')
+
+    if not pdf_file or not user_query:
+        return Response({"error": "Please provide both a PDF 'document' and a 'question'."}, status=400)
+
+    extracted_text = extract_text_from_pdf(pdf_file)
+    
+    if not extracted_text:
+        return Response({"error": "No text found. This PDF might be a scanned image. Please upload a text-based PDF."}, status=400)
+
+    try:
+        ai_answer = get_ai_response(extracted_text, user_query)
+        return Response({
+            "status": "success",
+            "question": user_query,
+            "answer": ai_answer
+        })
+    except Exception as e:
+        return Response({"error": f"AI processing failed: {str(e)}"}, status=500)
